cff1a0c04f779ff6c8888e48d4ed418877fdd68b
donghyunKang-dev <make_w@naver.com>
2022-10-12 23:53:02 +0900

    Feat: tailwind setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,12 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-        <input ref={inputRef} type="text" placeholder="name" />
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          ref={inputRef}
+          type="text"
+          placeholder="name"
+        />
         <button onClick={handleClick}>Add Jacket</button>
 
         <div>
