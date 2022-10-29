0ada496bfb5d3bf0e90dad71a064b1d63e32fe24
donghyunKang-dev <make_w@naver.com>
2022-10-29 19:14:22 +0900

    Feat: search

--- constants/products.ts
+++ constants/products.ts
@@ -1,2 +1,25 @@
 export const CATEGORY_MAP = ['Sneakers', 'T-shirt', 'Pants', 'Cap', 'Hoodie']
+
 export const TAKE = 9
+
+export const FILTERS = [
+  { label: '최신순', value: 'latest' },
+  { label: '가격 높은 순', value: 'expensive' },
+  { label: '가격 낮은 순', value: 'cheap' },
+]
+
+export const getOrderBy = (orderBy: string) => {
+  let orderByCondition
+  switch (orderBy) {
+    case 'cheap':
+      orderByCondition = { orderBy: { price: 'asc' } }
+      break
+    case 'expensive':
+      orderByCondition = { orderBy: { price: 'desc' } }
+      break
+    default:
+      orderByCondition = { orderBy: { createdAt: 'desc' } }
+      break
+  }
+  return orderBy ? orderByCondition : null
+}
--- pages/products/page.tsx
+++ pages/products/page.tsx
@@ -1,71 +1,118 @@
 import { categories, products } from '@prisma/client'
 import Image from 'next/image'
-import { Suspense, useEffect, useState } from 'react'
-import { Pagination, SegmentedControl } from '@mantine/core'
-import { CATEGORY_MAP, TAKE } from 'constants/products'
+import { useEffect, useState } from 'react'
+import { Input, Pagination, SegmentedControl, Select } from '@mantine/core'
+import { CATEGORY_MAP, FILTERS, TAKE } from 'constants/products'
+import { IconSearch } from '@tabler/icons'
+import useDebounce from 'hooks/useDebounce'
+import { useQuery } from '@tanstack/react-query'
 
 export default function Products() {
   const [activePage, setPage] = useState(1)
-  const [total, setTotal] = useState(0)
-  const [categories, setCategories] = useState<categories[]>([])
   const [selectedCategory, setCategory] = useState<string>('-1')
-  const [products, setProducts] = useState<products[]>([])
+  const [selectedFilter, setFilter] = useState<string | null>(FILTERS[0].value)
+  const [keyword, setKeyword] = useState<string>('')
 
-  useEffect(() => {
-    fetch(`/api/get-categories`)
-      .then((res) => res.json())
-      .then((data) => setCategories(data.items))
-  }, [])
+  const debouncedKeyword = useDebounce<string>(keyword)
 
-  useEffect(() => {
-    fetch(`/api/get-products-count?category=${selectedCategory}`)
-      .then((res) => res.json())
-      .then((data) => setTotal(Math.ceil(data.items / TAKE)))
-  }, [selectedCategory])
+  const { data: categories } = useQuery<
+    { items: categories[] },
+    unknown,
+    categories[]
+  >(
+    [`/api/get-categories`],
+    () => fetch(`/api/get-categories`).then((res) => res.json()),
+    {
+      select: (data) => data.items,
+    },
+  )
 
-  useEffect(() => {
-    const skip = TAKE * (activePage - 1)
-    fetch(
-      `/api/get-products?skip=${skip}&take=${TAKE}&category=${selectedCategory}`,
-    )
-      .then((res) => res.json())
-      .then((data) => setProducts(data.items))
-  }, [activePage, selectedCategory])
+  const { data: total } = useQuery(
+    [
+      `/api/get-products-count?category=${selectedCategory}&contains=${debouncedKeyword}`,
+    ],
+    () =>
+      fetch(
+        `/api/get-products-count?category=${selectedCategory}&contains=${debouncedKeyword}`,
+      )
+        .then((res) => res.json())
+        .then((data) => Math.ceil(data.items / TAKE)),
+  )
+
+  const { data: products } = useQuery<
+    { items: products[] },
+    unknown,
+    products[]
+  >(
+    [
+      `/api/get-products?skip=${
+        TAKE * (activePage - 1)
+      }&take=${TAKE}&category=${selectedCategory}&orderBy=${selectedFilter}&contains=${debouncedKeyword}`,
+    ],
+    () =>
+      fetch(
+        `/api/get-products?skip=${
+          TAKE * (activePage - 1)
+        }&take=${TAKE}&category=${selectedCategory}&orderBy=${selectedFilter}&contains=${debouncedKeyword}`,
+      ).then((res) => res.json()),
+    {
+      select: (data) => data.items,
+    },
+  )
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }
 
   return (
     <div className="px-36 mt-36 mb-36">
-      {categories && (
-        <div className="mb-4">
-          <SegmentedControl
-            value={selectedCategory}
-            onChange={setCategory}
-            data={[
-              { label: 'ALL', value: '-1' },
-              ...categories.map((category) => {
-                return { label: category.name, value: String(category.id) }
-              }),
-            ]}
-            color="dark"
-          />
-        </div>
-      )}
+      <div className="mb-4">
+        <Input
+          icon={<IconSearch />}
+          placeholder="Search Item You Want"
+          value={keyword}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4 flex justify-between align-middle">
+        {categories && (
+          <div className="mb-4">
+            <SegmentedControl
+              value={selectedCategory}
+              onChange={setCategory}
+              onClick={() => setPage(1)}
+              data={[
+                { label: 'ALL', value: '-1' },
+                ...categories.map((category) => {
+                  return { label: category.name, value: String(category.id) }
+                }),
+              ]}
+              color="dark"
+            />
+          </div>
+        )}
+        <Select value={selectedFilter} onChange={setFilter} data={FILTERS} />
+      </div>
       {products && (
         <div className="grid grid-cols-3 gap-5">
           {products.map((item) => {
             return (
-              <div key={item.id} style={{ maxWidth: 560 }}>
+              <div key={item.id}>
                 <Image
                   className="rounded"
                   src={item.image_url ?? ''}
-                  width={560}
-                  height={560}
+                  width={300}
+                  height={300}
+                  layout="responsive"
                   alt={item.name}
                   placeholder="blur"
                   blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg=="
                 />
-                <div className="flex">
-                  <span>{item.name}</span>
-                  <span className="ml-auto">{`${item.price.toLocaleString(
+                <div className="flex gap-4">
+                  <span className="whitespace-nowrap overflow-hidden text-ellipsis">
+                    {item.name}
+                  </span>
+                  <span className="ml-auto whitespace-nowrap">{`${item.price.toLocaleString(
                     'ko-KR',
                   )}원`}</span>
                 </div>
@@ -77,13 +124,21 @@ export default function Products() {
           })}
         </div>
       )}
+      {products && !products.length && keyword && (
+        <p className="mb-4">
+          {`'${keyword}'`}에 대한 검색 결과가 존재하지 않습니다.
+        </p>
+      )}
       <div className="w-full flex mt-5">
-        <Pagination
-          className="m-auto"
-          page={activePage}
-          onChange={setPage}
-          total={total}
-        />
+        {total && (
+          <Pagination
+            className="m-auto"
+            page={activePage}
+            onChange={setPage}
+            total={total}
+            color="dark"
+          />
+        )}
       </div>
     </div>
   )
